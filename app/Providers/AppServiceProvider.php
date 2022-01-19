52a26cac361f1e28f8930ca15edefa1d2c9c6f23
Clay <thejourney318@gmail.com>
2022-01-18 22:46:21 -0500

    ..

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,21 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Models\Category;
-use App\Models\Cartitem;
-use App\Models\Favorite;
-use App\Models\Item;
-use App\Models\User;
-use App\Models\Giftcard;
-
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-
-use Illuminate\Support\Facades\Session;
-
-use Illuminate\Support\Facades\View;
 
 use Illuminate\Http\Request;
 
@@ -31,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**

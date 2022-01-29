f6f274db51893435d27cd02590f2ce5aa4574dd0
Clay <thejourney318@gmail.com>
2022-01-29 04:23:05 -0500

    ..

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,8 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\GetCart::class,
-            \App\Http\Middleware\redirectSecureHttp::class
+            \App\Http\Middleware\GetCart::class
         ],
 
         'api' => [

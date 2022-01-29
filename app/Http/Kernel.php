571ec7b5450168c94ea87d23e510b6f0b66fbf87
Clay <thejourney318@gmail.com>
2022-01-29 04:05:17 -0500

    ..

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\GetCart::class,
+            \App\Http\Middleware\HttpsProtocolMiddleware::class
         ],
 
         'api' => [

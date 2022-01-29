ab2fcd3f8e269d04fce0e5462e73535b072454b7
Clay <thejourney318@gmail.com>
2022-01-29 03:55:07 -0500

    ..

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = 'https://www.restaurant18.herokuapp.com/dashboard';
 
     /**
      * The controller namespace for the application.

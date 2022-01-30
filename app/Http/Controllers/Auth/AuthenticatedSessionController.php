f8624338f5c7e3d9e996f98bcb930f0ffb03b96d
Clay <thejourney318@gmail.com>
2022-01-29 22:30:09 -0500

    ..

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -110,7 +110,7 @@ class AuthenticatedSessionController extends Controller
                     ]);
                     $reward->save();
                 }
-                $code = null;
+                $code = NULL;
             }
         }
         $user->save();

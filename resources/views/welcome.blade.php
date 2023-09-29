9482417e3c56b6775cffee283e9ced91926f5480
Clay <thejourney318@gmail.com>
2023-09-29 05:04:13 -0400

    Decreased font-size on buttons on home favorites slider on mobile.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,16 @@
                             <div class='fav-content'>              
                                 <h4>{{$favorite->name}}</h4>
                                 <p>{{$favorite->description}}</p>
-                                <button>Add To Order</button>
+                                <form method="POST" action="/items" class='addToCart col-11' name='cart-form'>
+                                    @csrf
+                                    <input type ='hidden' name='name' value='{{$favorite->name}}' class='name'>
+                                    <input type ='hidden' name='image_url' value='{{$favorite->image_url}}' class='image_url'>
+                                    <input type ='hidden' name='description' value='{{$favorite->description}}' class='description'>
+                                    <input type ='hidden' name='price' value='{{$favorite->price}}' class='price'>
+                                    <input type ='hidden' name='category' value='{{$favorite->category->name}}' class='category'>
+                                    <input type ='hidden' name='id' value='{{$favorite->id}}' class='id'>
+                                    <input type='submit' class='order' name='order' value='Add To Cart'>
+                                </form>
                             </div>
                         </li>
                     @endforeach
@@ -27,102 +36,6 @@
                 <div class='circle-3 mx-2'></div>
                 <div class='circle-4 mx-2'></div>
             </div>
-        {{-- @elseif(Auth::user() && count($user->favorites) == 0)
-            <h2>Guy's Favorites</h2>
-            <div class='favs-container mt-4'>
-                <ul class='fav-slider'>
-                    <li class='my-favs'>
-                        @if(App::environment('production'))
-                            <img src='{{ asset('/img/burger-2.webp') }}'> 
-                        @else
-                            <img src='{{ asset('/img/burger-2.webp') }}'>
-                        @endif
-                        <div class='fav-content'>              
-                            <h4>Mushroom Swiss Burger</h4>
-                            <p>Pure beef topped off with mushrooms, sauteed onions, Swiss cheese, lettuce, tomato, and garlic aioli.</p>
-                            <button>Add To Order</button>
-                        </div>
-                    </li>
-                    <li class='my-favs'>
-                        @if(App::environment('production'))
-                            <img src='{{ asset('/img/pasta-1.webp') }}'> 
-                        @else
-                            <img src='{{ asset('/img/pasta-1.webp') }}'>
-                        @endif                    
-                        <div class='fav-content'>
-                            <h4>Chicken Alfredo</h4>
-                            <p>Fettucini pasta covered in delicious alfredo sauce and sautéed shrimp.</p>
-                            <button>Add To Order</button>
-                        </div>
-                    </li>
-                    <li class='my-favs'>
-                        @if(App::environment('production'))
-                            <img src='{{ asset('/img/steak-1.webp') }}'>
-                        @else
-                            <img src='{{ asset('/img/steak-1.webp') }}'>
-                        @endif                         
-                        <div class='fav-content'>
-                            <h4>Ribeye Steak</h4>
-                            <p>Thick-cut steak topped with garlic butter.</p>
-                            <button>Add To Order</button>
-                        </div>
-                    </li>
-                    <li class='my-favs'>
-                    @if(App::environment('production'))
-                        <img src='{{ asset('/img/appetizer-3.webp') }}'>
-                    @else
-                        <img src='{{ asset('/img/appetizer-3.webp') }}'>
-                    @endif   
-                        <div class='fav-content'>              
-                            <h4>Boneless Wings</h4>
-                            <p>Bone out, taste in.</p>
-                            <button>Add To Order</button>
-                        </div>
-                    </li>
-                    <li class='my-favs'>
-                    @if(App::environment('production'))
-                        <img src='{{ asset('/img/appetizer-1.webp') }}'>
-                    @else
-                        <img src='{{ asset('/img/appetizer-1.webp') }}'>
-                    @endif 
-                        <div class='fav-content'>
-                            <h4>Mozzarella Sticks</h4>
-                            <p>Stuffed with cheese, served with marinara sauce.</p>
-                            <button>Add To Order</button>
-                        </div>
-                    </li>
-                    <li class='my-favs'>
-                        @if(App::environment('production'))
-                            <img src='{{ asset('/img/pasta-5.webp') }}'>
-                        @else
-                            <img src='{{ asset('/img/pasta-5.webp') }}'>
-                        @endif 
-                        <div class='fav-content'>
-                            <h4>Cheese Ravioli</h4>
-                            <p>Filled with a decadent blend of Italian cheeses, topped with your choice of marinara or a meat sauce, along with melted mozzarella.</p>
-                            <button>Add To Order</button>
-                        </div>
-                    </li>
-                    <li class='my-favs'>
-                        @if(App::environment('production'))
-                            <img src='{{ asset('/img/steak-2.webp') }}'>
-                        @else
-                            <img src='{{ asset('/img/steak-2.webp') }}'>
-                        @endif 
-                        <div class='fav-content'>
-                            <h4>Sirloin Steak - 10oz</h4>
-                            <p>Seasoned & topped with garlic butter.</p>
-                            <button>Add To Order</button>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <div class='nav-circles'>
-                <div class='circle-1 mx-2'></div>
-                <div class='circle-2 mx-2'></div>
-                <div class='circle-3 mx-2'></div>
-                <div class='circle-4 mx-2'></div>
-            </div> --}}
         @else
             <h2>Guy's Favorites</h2>
             <div class='favs-container mt-4'>
@@ -146,7 +59,16 @@
                         <div class='fav-content'>              
                             <h4>Mushroom Swiss Burger</h4>
                             <p>Pure beef topped off with mushrooms, sauteed onions, Swiss cheese, lettuce, tomato, and garlic aioli.</p>
-                            <button>Add To Order</button>
+                            <form method="POST" action="/items" class='addToCart col-11' name='cart-form'>
+                                @csrf
+                                <input type ='hidden' name='name' value='{{$favorite->name}}' class='name'>
+                                <input type ='hidden' name='image_url' value='{{$favorite->image_url}}' class='image_url'>
+                                <input type ='hidden' name='description' value='{{$favorite->description}}' class='description'>
+                                <input type ='hidden' name='price' value='{{$favorite->price}}' class='price'>
+                                <input type ='hidden' name='category' value='{{$favorite->category->name}}' class='category'>
+                                <input type ='hidden' name='id' value='{{$favorite->id}}' class='id'>
+                                <input type='submit' class='order' name='order' value='Add To Cart'>
+                            </form>
                         </div>
                     </li>
                     <li class='my-favs'>
@@ -158,7 +80,16 @@
                         <div class='fav-content'>
                             <h4>Chicken Alfredo</h4>
                             <p>Fettucini pasta covered in delicious alfredo sauce and sautéed shrimp.</p>
-                            <button>Add To Order</button>
+                            <form method="POST" action="/items" class='addToCart col-11' name='cart-form'>
+                                @csrf
+                                <input type ='hidden' name='name' value='{{$favorite->name}}' class='name'>
+                                <input type ='hidden' name='image_url' value='{{$favorite->image_url}}' class='image_url'>
+                                <input type ='hidden' name='description' value='{{$favorite->description}}' class='description'>
+                                <input type ='hidden' name='price' value='{{$favorite->price}}' class='price'>
+                                <input type ='hidden' name='category' value='{{$favorite->category->name}}' class='category'>
+                                <input type ='hidden' name='id' value='{{$favorite->id}}' class='id'>
+                                <input type='submit' class='order' name='order' value='Add To Cart'>
+                            </form>
                         </div>
                     </li>
                     <li class='my-favs'>

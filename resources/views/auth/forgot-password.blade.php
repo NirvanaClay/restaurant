e3864564ce5da1a7c4e4bc89f1ed93b0054c49d9
Clay <thejourney318@gmail.com>
2023-11-10 03:44:00 -0500

    Centers error messages on password reset form.

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -9,10 +9,10 @@
             </div>
 
             <!-- Session Status -->
-            <x-auth-session-status class="mb-4" :status="session('status')" />
+            <x-auth-session-status class="mb-4 mx-auto" :status="session('status')" />
 
             <!-- Validation Errors -->
-            <x-auth-validation-errors class="mb-4" :errors="$errors" />
+            <x-auth-validation-errors class="mb-4 mx-auto" :errors="$errors" />
 
             <form method="POST" action="{{ route('password.email') }}">
                 @csrf
